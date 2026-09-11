44a020e665f3d9e5cedea06df39b3b5e13cb5720
Mason Schultz <youngpapaskid@gmail.com>
2026-09-11 14:06:08 -0400

    Create GenePriority.ts

---
+++ tsc/GenePriority.ts
@@ -0,0 +1,90 @@
+export enum BaseColor {
+  B,
+  b,
+  b1,
+}
+export const getBaseColor = ['B', 'b', 'b1'];
+
+export enum Scale {
+  T,
+  t,
+}
+export const getScale = ['T', 't'];
+
+export enum MultiColorism {
+  R,
+  r1,
+  r2,
+  r3,
+}
+export const getMultiColorism = ['R', 'r1', 'r2', 'r3'];
+
+export enum Dilution {
+  O,
+  o,
+}
+export const getDilution = ['O', 'o'];
+
+export enum Point {
+  P,
+  pw,
+}
+export const getPoint = ['P', 'pr', 'ps', 'pw'];
+
+export enum Silver {
+  W,
+  w,
+  wn,
+}
+export const getSilver = ['W', 'w', 'wn'];
+
+export enum GildPattern {
+  L,
+  l,
+}
+export const getGildPattern = ['L', 'l'];
+
+export enum Depth {
+  D,
+  d,
+}
+export const getDepth = ['D', 'd'];
+
+export enum Breaking {
+  Z,
+  z,
+  z1,
+}
+export const getBreaking = ['Z', 'z', 'z1'];
+
+export enum Albinism {
+  C,
+  c,
+}
+export const getAlbinism = ['C', 'c'];
+
+export enum AccentingColor {
+  A,
+  a,
+  a1,
+  a2,
+}
+export const getAccentingColor = ['A', 'a', 'a1', 'a2'];
+
+export enum AccentGradient {
+  Ag,
+  ag,
+}
+export const getAccentGradient = ['Ag', 'ag'];
+
+export enum AccentEyespot {
+  Ae,
+  ae,
+}
+export const getAccentEyespot = ['Ae', 'ae'];
+
+export enum AccentingFin {
+  Af,
+  af,
+}
+export const getAccentFin = ['Af', 'af'];
