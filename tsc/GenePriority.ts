a9d4efcd54de3dd42bca91b2526baa84b88ef736
Mason S <132116525+ElizaAlex@users.noreply.github.com>
2026-09-11 15:59:33 -0400

    Bookmark

--- tsc/GenePriority.ts
+++ tsc/GenePriority.ts
@@ -1,90 +1,78 @@
-export enum BaseColor {
-  B,
-  b,
-  b1,
-}
-export const getBaseColor = ['B', 'b', 'b1'];
+namespace Genes {
+  export enum BaseColor {
+    B,
+    b,
+    b1,
+  }
 
-export enum Scale {
-  T,
-  t,
-}
-export const getScale = ['T', 't'];
+  export enum Scale {
+    T,
+    t,
+  }
+  export enum MultiColorism {
+    R,
+    r1,
+    r2,
+    r3,
+  }
 
-export enum MultiColorism {
-  R,
-  r1,
-  r2,
-  r3,
-}
-export const getMultiColorism = ['R', 'r1', 'r2', 'r3'];
+  export enum Dilution {
+    O,
+    o,
+  }
 
-export enum Dilution {
-  O,
-  o,
-}
-export const getDilution = ['O', 'o'];
+  export enum Point {
+    P,
+    pw,
+  }
 
-export enum Point {
-  P,
-  pw,
-}
-export const getPoint = ['P', 'pr', 'ps', 'pw'];
+  export enum Silver {
+    W,
+    w,
+    wn,
+  }
 
-export enum Silver {
-  W,
-  w,
-  wn,
-}
-export const getSilver = ['W', 'w', 'wn'];
+  export enum GildPattern {
+    L,
+    l,
+  }
 
-export enum GildPattern {
-  L,
-  l,
-}
-export const getGildPattern = ['L', 'l'];
+  export enum Depth {
+    D,
+    d,
+  }
 
-export enum Depth {
-  D,
-  d,
-}
-export const getDepth = ['D', 'd'];
+  export enum Breaking {
+    Z,
+    z,
+    z1,
+  }
 
-export enum Breaking {
-  Z,
-  z,
-  z1,
-}
-export const getBreaking = ['Z', 'z', 'z1'];
+  export enum Albinism {
+    C,
+    c,
+  }
 
-export enum Albinism {
-  C,
-  c,
-}
-export const getAlbinism = ['C', 'c'];
+  export enum AccentingColor {
+    A,
+    a,
+    a1,
+    a2,
+  }
 
-export enum AccentingColor {
-  A,
-  a,
-  a1,
-  a2,
-}
-export const getAccentingColor = ['A', 'a', 'a1', 'a2'];
+  export enum AccentGradient {
+    Ag,
+    ag,
+  }
 
-export enum AccentGradient {
-  Ag,
-  ag,
-}
-export const getAccentGradient = ['Ag', 'ag'];
+  export enum AccentEyespot {
+    Ae,
+    ae,
+  }
 
-export enum AccentEyespot {
-  Ae,
-  ae,
-}
-export const getAccentEyespot = ['Ae', 'ae'];
+  export enum AccentingFin {
+    Af,
+    af,
+  }
 
-export enum AccentingFin {
-  Af,
-  af,
 }
-export const getAccentFin = ['Af', 'af'];
--- tsc/Gener.ts
+++ tsc/Gener.ts
@@ -1,29 +1,48 @@
-import { randomInt } from 'node:crypto';
-import * as Genes from './GenePriority';
-
 type GeneTuple<T> = [T, T] | undefined;
 
 type GeneSet = {
-  baseColors: GeneTuple<Genes.BaseColor>;
-  scale: GeneTuple<Genes.Scale>;
-  multiColor: GeneTuple<Genes.MultiColorism>;
-  dilution: GeneTuple<Genes.Dilution>;
-  point: GeneTuple<Genes.Point>;
-  silver: GeneTuple<Genes.Silver>;
-  gild: GeneTuple<Genes.GildPattern>;
-  depth: GeneTuple<Genes.Depth>;
-  breaking: GeneTuple<Genes.Breaking>;
-  albino: GeneTuple<Genes.Albinism>;
-  accentColor: GeneTuple<Genes.AccentingColor>;
-  accentGradient: GeneTuple<Genes.AccentGradient>;
-  accentEyespot: GeneTuple<Genes.AccentEyespot>;
-  accentFin: GeneTuple<Genes.AccentingFin>;
+  BaseColor: GeneTuple<Genes.BaseColor>;
+  Scale: GeneTuple<Genes.Scale>;
+  MultiColorism: GeneTuple<Genes.MultiColorism>;
+  Dilution: GeneTuple<Genes.Dilution>;
+  Point: GeneTuple<Genes.Point>;
+  Silver: GeneTuple<Genes.Silver>;
+  GildPattern: GeneTuple<Genes.GildPattern>;
+  Depth: GeneTuple<Genes.Depth>;
+  Breaking: GeneTuple<Genes.Breaking>;
+  Albino: GeneTuple<Genes.Albinism>;
+  AccentingColor: GeneTuple<Genes.AccentingColor>;
+  AccentGradient: GeneTuple<Genes.AccentGradient>;
+  AccentEyespot: GeneTuple<Genes.AccentEyespot>;
+  AccentFin: GeneTuple<Genes.AccentingFin>;
 };
+
+const parents: GeneSet[] = [createBlankGeneSet(), createBlankGeneSet()];
+
+function createBlankGeneSet(): GeneSet {
+  let temp = {};
+  return {
+    BaseColor: [0,0],
+    Scale: [0,0],
+    MultiColorism:[0,0],
+    Dilution:[0,0],
+    Point: [0,0],
+    Silver:[0,0],
+    GildPattern:[0,0],
+    Depth:[0,0],
+    Breaking:[0,0],
+    Albino:[0,0],
+    AccentingColor:[0,0],
+    AccentGradient:[0,0],
+    AccentEyespot: [0,0],
+    AccentFin:[0,0],
+  }
+}
 
 function getRandomGene<T>(genes: GeneTuple<T>): T {
   if (genes === undefined) throw new Error('Genes must be defined');
 
-  return genes[randomInt(0, 1)];
+  return genes[Math.floor(Math.random() * genes.length)];
 }
 
 function InheritGene<T>(g1: GeneTuple<T>, g2: GeneTuple<T>): GeneTuple<T> {
@@ -35,35 +54,84 @@ function InheritGene<T>(g1: GeneTuple<T>, g2: GeneTuple<T>): GeneTuple<T> {
 
 function breed(genes1: GeneSet, genes2: GeneSet): GeneSet {
   return {
-    baseColors: InheritGene(genes1.baseColors, genes2.baseColors),
-    scale: InheritGene(genes1.scale, genes2.scale),
-    multiColor: InheritGene(genes1.multiColor, genes2.multiColor),
-    dilution: InheritGene(genes1.dilution, genes2.dilution),
-    point: InheritGene(genes1.point, genes2.point),
-    silver: InheritGene(genes1.silver, genes2.silver),
-    gild: InheritGene(genes1.gild, genes2.gild),
-    depth: InheritGene(genes1.depth, genes2.depth),
-    breaking: InheritGene(genes1.breaking, genes2.breaking),
-    albino: InheritGene(genes1.albino, genes2.albino),
-    accentColor: InheritGene(genes1.accentColor, genes2.accentColor),
-    accentGradient: InheritGene(genes1.accentGradient, genes2.accentGradient),
-    accentEyespot: InheritGene(genes1.accentEyespot, genes2.accentEyespot),
-    accentFin: InheritGene(genes1.accentFin, genes2.accentFin),
+    BaseColor: InheritGene(genes1.BaseColor, genes2.BaseColor),
+    Scale: InheritGene(genes1.Scale, genes2.Scale),
+    MultiColorism: InheritGene(genes1.MultiColorism, genes2.MultiColorism),
+    Dilution: InheritGene(genes1.Dilution, genes2.Dilution),
+    Point: InheritGene(genes1.Point, genes2.Point),
+    Silver: InheritGene(genes1.Silver, genes2.Silver),
+    GildPattern: InheritGene(genes1.GildPattern, genes2.GildPattern),
+    Depth: InheritGene(genes1.Depth, genes2.Depth),
+    Breaking: InheritGene(genes1.Breaking, genes2.Breaking),
+    Albino: InheritGene(genes1.Albino, genes2.Albino),
+    AccentingColor: InheritGene(genes1.AccentingColor, genes2.AccentingColor),
+    AccentGradient: InheritGene(genes1.AccentGradient, genes2.AccentGradient),
+    AccentEyespot: InheritGene(genes1.AccentEyespot, genes2.AccentEyespot),
+    AccentFin: InheritGene(genes1.AccentFin, genes2.AccentFin),
   };
 }
 
-// const test = async () => {
-//   const gene1: GeneTuple<Genes.BaseColor> = [
-//     Genes.BaseColor.B,
-//     Genes.BaseColor.b1,
-//   ];
-//   const gene2: GeneTuple<Genes.BaseColor> = [
-//     Genes.BaseColor.b,
-//     Genes.BaseColor.b1,
-//   ];
-//
-//   const out = InheritGene(gene1, gene2);
-//   console.log(out);
-// };
-//
-// test();
+function getCommonName(s: string): string {
+  return s.split(/(?=[A-Z])/).join(' ');
+}
+
+function getGeneOptions(s:string, id: number) {
+  return Object.keys(Genes[s as keyof typeof Genes] as object).map(i => {
+    if(parseInt(i) != (i as unknown as number)) return `
+      <option value="${i}">${i}</option>
+    `
+  }).join('');
+}
+
+const GenParentHTML = (elementId: string, parent: 0 | 1) => {
+  const container = document.getElementById(elementId);
+
+  if (!container) {
+    console.error('No container found');
+    return;
+  }
+  console.log('Container found.');
+
+
+  const getId = (gene: string, i: number) => `${gene}p${parent}g${i}`;
+
+
+
+
+  container.innerHTML = Object.keys(Genes).map(gene => {
+    const setGenes = () => {
+      if (!document) return;
+      const p : GeneSet = parents[parent];
+      const g0 = document.getElementById(getId(gene,0))?.innerHTML;
+      const g1 = document.getElementById(getId(gene,1))?.innerHTML;
+
+      if (g0 === undefined || g1 === undefined) throw new Error('Could not find element: ' + gene);
+
+      try {
+        const Gene = Genes[gene as unknown as keyof typeof Genes]
+        const G0 = Gene[g0 as keyof typeof Gene];
+        const G1 = Gene[g1 as keyof typeof Gene];
+        p[]
+      }
+
+
+
+    };
+    return `
+        <div class="item-card">
+            <h3 class="item-title">${getCommonName(gene)}</h3>
+            <label for="genes">Choose genes</label>
+            <select name="genes" id = ${getId(gene,0)} onchange = "setGenes()">
+                ${getGeneOptions(gene, 0)}
+            </select>
+            <select name="genes" id = ${getId(gene,0)} onchange = "setGenes()">
+                ${getGeneOptions(gene, 1)}
+            </select>
+        </div>
+  `
+  }).join('');
+}
+
+GenParentHTML('parent-1', 0);
+GenParentHTML('parent-2', 1);
+
